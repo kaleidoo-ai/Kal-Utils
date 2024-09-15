e4110dfa2bf22bccb12f78ac78922188fd178cd7
בר הורוביץ <brhwrwbyz@brs-MacBook-Pro.local>
2024-09-15 11:54:19 +0300

    storage

--- kal_utils/__init__.py
+++ kal_utils/__init__.py
@@ -1,6 +1,8 @@
 """Top-level package for kal-utils."""
 # kal_utils/__init__.py
-from . import requests, logger, handle_response, mongodb, time_zone, bucket, helper
+from . import requests, logger, handle_response, mongodb, time_zone, bucket, helper, storage
 __author__ = """Bar Lander"""
 __email__ = "barh@kaleidoo.ai"
-__version__ = "2.0.4"
+__version__ = "2.0.5"
+
+__all__ = ['requests', 'logger', 'handle_response', 'mongodb', 'time_zone', 'bucket', 'helper', 'storage']
---
+++ kal_utils/storage/__init__.py
@@ -0,0 +1,17 @@
+import os
+from .base_storage import  BaseStorage
+from .gcs_storage import GCSStorage
+from .minio_storage import MinIOStorage
+
+__all__ = ['BaseStorage', 'GCSStorage', 'MinIOStorage']
+
+def get_storage_client(credentials_json = None) -> BaseStorage:
+    storage_type = os.environ.get('STORAGE_TYPE', 'GCS').upper()
+    if storage_type == 'GCS':
+        return GCSStorage(credentials_json)
+    elif storage_type == 'MINIO':
+        return MinIOStorage(credentials_json)
+    else:
+        raise ValueError(f"Unsupported storage type: {storage_type}")
+
+
