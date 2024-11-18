73a1e14e5d3ecf6aea357193217321986f3b69ab
yoav <yoav.kitai@kaleidoo.ai>
2024-11-18 14:11:11 +0200

    Changed version to 2.0.8.12

--- kal_utils/__init__.py
+++ kal_utils/__init__.py
@@ -3,7 +3,7 @@
 from . import requests, logger, handle_response, mongodb, time_zone, helper, monitoring
 __author__ = """Bar Lander"""
 __email__ = "barh@kaleidoo.ai"
-__version__ = "2.0.8.11"
+__version__ = "2.0.8.12"
 
 
 __all__ = ['requests', 'logger', 'handle_response', 'mongodb', 'time_zone', 'helper', 'storage', 'monitoring']
